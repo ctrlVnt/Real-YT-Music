74b8b9cd4b5fe442cd7a0f0c3e20b7d04bc3c9fa
ctrlVnt <gordanobruno227@gmail.com>
2025-02-19 18:19:18 +0100

    Changed info screen

--- app/src/main/java/com/ctrlvnt/rytm/ui/fragment/Settings.kt
+++ app/src/main/java/com/ctrlvnt/rytm/ui/fragment/Settings.kt
@@ -1,5 +1,8 @@
 package com.ctrlvnt.rytm.ui.fragment
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
@@ -9,10 +12,9 @@ import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.ctrlvnt.rytm.R
-import com.ctrlvnt.rytm.ui.MainActivity
+
 
 class Settings : Fragment(){
     override fun onCreateView(
@@ -21,9 +23,32 @@ class Settings : Fragment(){
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_settings, container, false)
         val backButton: ImageButton = rootView.findViewById(R.id.back_button)
+        val buymeacoffeeButton : Button = rootView.findViewById(R.id.buymeacoffee)
+        val sendmemailButton : Button = rootView.findViewById(R.id.sendmeamail)
 
         backButton.setOnClickListener{
             requireActivity().supportFragmentManager.popBackStack()
+        }
+
+        buymeacoffeeButton.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://buymeacoffee.com/v3ntuz"))
+            startActivity(browserIntent)
+        }
+
+        sendmemailButton.setOnClickListener {
+            val i = Intent(Intent.ACTION_SEND)
+            i.setType("message/rfc822")
+            i.putExtra(Intent.EXTRA_EMAIL, arrayOf("giordanobruno227@gmail.com"))
+            i.putExtra(Intent.EXTRA_SUBJECT, "I have a suggestion for RYTM!")
+            try {
+                startActivity(Intent.createChooser(i, "Send mail..."))
+            } catch (ex: ActivityNotFoundException) {
+                Toast.makeText(
+                    context,
+                    "There are no email clients installed.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
 
         val mTextView: TextView = rootView.findViewById(R.id.my_info)
