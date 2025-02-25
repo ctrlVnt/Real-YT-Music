f660a426b01ba5f7b0baefcfe0d7bdeca2c5cd4b
ctrlVnt <gordanobruno227@gmail.com>
2025-02-26 00:01:48 +0100

    2.1 hindi added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ctrlvnt.rytm"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "2.0"
+        versionCode = 11
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
