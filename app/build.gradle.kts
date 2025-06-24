e04adf207a29a41f7e4bc7518de285ccc1dd1e65
ctrlVnt <gordanobruno227@gmail.com>
2025-06-24 17:14:21 +0200

    V3.2 - F-Droid compatibility fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ctrlvnt.rytm"
         minSdk = 24
         targetSdk = 34
-        versionCode = 21
-        versionName = "3.1"
+        versionCode = 22
+        versionName = "3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
