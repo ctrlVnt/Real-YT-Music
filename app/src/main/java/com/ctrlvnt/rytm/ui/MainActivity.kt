e15cff1bf4d4ccbfdee8f4c954a3ea22b6a40f00
Riccardo Venturini <Giordanobruno227@gmail.com>
2026-08-26 23:13:24 +0200

    bottom bar adaptation

--- app/src/main/java/com/ctrlvnt/rytm/ui/MainActivity.kt
+++ app/src/main/java/com/ctrlvnt/rytm/ui/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.app.ActivityCompat
 import androidx.core.content.edit
 import androidx.core.net.toUri
+import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
@@ -54,6 +55,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)!!
+
+        ViewCompat.setOnApplyWindowInsetsListener(bottomNav) { view, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.navigationBars())
+
+            val density = view.resources.displayMetrics.density
+            val baseHeightPx = (80 * density).toInt()
+
+            val layoutParams = view.layoutParams
+            layoutParams.height = baseHeightPx + insets.bottom
+            view.layoutParams = layoutParams
+
+            view.setPadding(0, 0, 0, insets.bottom)
+
+            windowInsets
+        }
+
         bottomNav.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.home -> {
