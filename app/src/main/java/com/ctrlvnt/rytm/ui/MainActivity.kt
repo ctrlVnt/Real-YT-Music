998db00a0cfef18d2f7ba32e886a32869d50072e
Riccardo Venturini <Giordanobruno227@gmail.com>
2026-09-24 10:09:08 +0200

    v8.2 - solved crash

--- app/src/main/java/com/ctrlvnt/rytm/ui/MainActivity.kt
+++ app/src/main/java/com/ctrlvnt/rytm/ui/MainActivity.kt
@@ -289,12 +289,7 @@ class MainActivity : AppCompatActivity() {
                 """
             You’ve just installed a new version! Here’s what’s new:
             
-            • library update
-            • New issue management for bug reports
-            
-            Thank you for your donations❤️
-            - LP🫶
-            - Ceryus🫶
+            • bug fixing
            
             """.trimIndent()
             )
